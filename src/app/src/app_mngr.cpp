20e549bfa087ab101462815c99f9498eae67c9b0
sengulhamza <sengulhamza@icloud.com>
2024-03-25 15:40:31 +0300

    RGB LedC manager added

--- src/app/src/app_mngr.cpp
+++ src/app/src/app_mngr.cpp
@@ -31,6 +31,7 @@
 #include "app/app_config.h"
 #include "app/app_types.h"
 #include "app/tof_mngr.h"
+#include "drv/rgb_ledc_controller.h"
 
 static const char *TAG = "app_mngr";
 
@@ -87,6 +88,54 @@ esp_err_t app_start(void)
 
     esp_err_t status = ESP_OK;
     status |= tof_mngr_init();
+
+
+// Define the GPIO pins for the Red, Green, and Blue LEDs
+#define GPIO_LED_RED    (GPIO_NUM_26)
+#define GPIO_LED_GREEN  (GPIO_NUM_27)
+#define GPIO_LED_BLUE   (GPIO_NUM_25)
+
+// Define colors as RGB values in hexadecimal
+#define RED     0xFF0000 // Red color
+#define GREEN   0x00FF00 // Green color
+#define BLUE    0x0000FF // Blue color
+#define YELLOW  0xFFFF00 // Yellow color (combination of Red and Green)
+#define VIOLET  0xEE82EE // Violet color
+
+    // Create an instance of the RGB LED structure and initialize it with the defined GPIO pins and LEDC channels
+    rgb_led_t led1 = rgb_led_new(GPIO_LED_RED, GPIO_LED_GREEN, GPIO_LED_BLUE, LEDC_CHANNEL_0, LEDC_CHANNEL_1, LEDC_CHANNEL_2);
+    //...you can add one more eg: led2.It is limited to 2 leds due to the LEDC module having only 8 channels available.
+
+    // Initialize the RGB LED, check for any errors during the process
+    ESP_ERROR_CHECK(rgb_led_init(&led1));
+
+    // Main loop of the program
+    while (true) {
+
+        // Start a blinking effect with the Green color. Blink 3 times, each blink lasts 500ms, and the time between blinks is 500ms. The total effect duration is 3000ms.
+        rgb_led_start_blink_effect(&led1, RED, 3, 500, 500, 3000);
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Delay for 5 seconds
+
+
+        // Start a blinking effect with the Green color. Blink 3 times, each blink lasts 500ms, and the time between blinks is 500ms. The total effect duration is 3000ms.
+        rgb_led_start_blink_effect(&led1, GREEN, 3, 500, 500, 3000);
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Delay for 5 seconds
+
+        // Start a blinking effect with the Green color. Blink 3 times, each blink lasts 500ms, and the time between blinks is 500ms. The total effect duration is 3000ms.
+        rgb_led_start_blink_effect(&led1, BLUE, 3, 500, 500, 3000);
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Delay for 5 seconds
+
+
+        // Start a blinking effect with the Yellow color. Blink 3 times, each blink lasts 500ms, and the time between blinks is 500ms. The total effect duration is 3000ms.
+        rgb_led_start_blink_effect(&led1, YELLOW, 3, 500, 500, 3000);
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Delay for 5 seconds
+
+        // Set the LED color Fixed to Violet
+        rgb_led_set_color(&led1, YELLOW);
+
+        rgb_led_start_transition_effect(&led1, YELLOW, RED, 10, NULL);
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Delay for 5 seconds
+    }
 
 #ifdef DEBUG_BUILD
     print_heap_usage("after first init");
