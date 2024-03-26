164c3562e278ea7e1ca91a558cc205b5ef9e3364
sengulhamza <sengulhamza@icloud.com>
2024-03-26 19:02:02 +0300

    CAN Bus frame structures added

--- src/app/src/canbus_mngr.c
+++ src/app/src/canbus_mngr.c
@@ -33,13 +33,12 @@
 #include "app/app_types.h"
 
 static const char *TAG = "canbus_mngr";
-
-
-#define TX_GPIO_NUM             0
-#define RX_GPIO_NUM             15
-#define TX_TASK_PRIO            8       //Sending task priority
-#define RX_TASK_PRIO            9       //Receiving task priority
-#define MSG_ID                  0x111   //11 bit standard format ID
+#define CANBUS_MNGR_TX_PERIOD_MS    (100)
+#define TX_GPIO_NUM                 (0)
+#define RX_GPIO_NUM                 (15)
+#define TX_TASK_PRIO                (8)       //Sending task priority
+#define RX_TASK_PRIO                (9)       //Receiving task priority
+#define MSG_ID                      (0x111)   //11 bit standard format ID
 
 static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
 //Filter all other IDs except MSG_ID
@@ -50,20 +49,58 @@ static const twai_filter_config_t f_config = {.acceptance_code = (MSG_ID << 21),
 //Set to NO_ACK mode due to self testing with single module
 static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TX_GPIO_NUM, RX_GPIO_NUM, TWAI_MODE_NORMAL);
 
+typedef union {
+    struct {
+        uint16_t current_error : 1;
+        uint16_t software_error : 1;
+        uint16_t first_reset : 1;
+        uint16_t reserved : 13;
+    };
+    uint16_t word;
+} state_union_t;
+
+typedef union {
+    struct {
+        uint16_t no_color : 1;
+        uint16_t red : 1;
+        uint16_t green : 1;
+        uint16_t blue : 1;
+        uint16_t yellow : 1;
+        uint16_t reserved : 11;
+    };
+    uint16_t word;
+} rgb_status_union_t;
+
+typedef struct {
+    float val;
+} distance_tof_t;
+
+typedef struct {
+    state_union_t state;
+    rgb_status_union_t rgb_status;
+    distance_tof_t distance;
+} __attribute__((packed)) can_comm_frame_t;
+
 static void canbus_tx_task(void *arg)
 {
     ESP_LOGI(TAG, "%s started!", __func__);
     twai_message_t tx_msg = {.data_length_code = 8, .identifier = MSG_ID};
-    uint8_t i = 0;
     while (pdTRUE) {
         //twai_start();
+
+        /**
+         * @brief Prepare CAN Bus message here.
+         *
+         */
+        can_comm_frame_t can_tx_msg = {0};
+
         //Transmit messages using self reception request
-        tx_msg.data[0] = i++;
+        memcpy(tx_msg.data, &can_tx_msg, tx_msg.data_length_code);
         if (twai_transmit(&tx_msg, 100) != ESP_OK) {
-            ESP_LOGE(TAG, "TX ERROR!");
+            ESP_LOGE(TAG, "CAN BUS TX ERROR!");
             twai_clear_transmit_queue();
         } else {
-            ESP_LOGI(TAG, "Msg sent - Data = %d", tx_msg.data[0]);
+            ESP_LOGI(TAG, "CAN Bus Message sent - Data: [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] ", tx_msg.data[0], tx_msg.data[1], tx_msg.data[2], tx_msg.data[3], tx_msg.data[4], tx_msg.data[5], tx_msg.data[6], tx_msg.data[7]);
         }
         //twai_stop();
         vTaskDelay(pdMS_TO_TICKS(50));
@@ -75,10 +112,14 @@ static void canbus_rx_task(void *arg)
 {
     ESP_LOGI(TAG, "%s started!", __func__);
     twai_message_t rx_message;
+    can_comm_frame_t can_rx_msg = {0};
     while (pdTRUE) {
         //Receive message and print message data
         if (twai_receive(&rx_message, portMAX_DELAY) == ESP_OK) {
-            ESP_LOGI(TAG, "Msg received - Data = %d", rx_message.data[0]);
+            ESP_LOGI(TAG, "CAN Bus Message sent - Data: [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] [0x%x] ", rx_message.data[0], rx_message.data[1], rx_message.data[2], rx_message.data[3], rx_message.data[4], rx_message.data[5], rx_message.data[6], rx_message.data[7]);
+            if (rx_message.data_length_code == sizeof(can_rx_msg)) {
+                memcpy(&can_rx_msg, rx_message.data, rx_message.data_length_code);
+            }
         }
         vTaskDelay(pdMS_TO_TICKS(10));
     }
